5514eee3b38aed32e97492c6bf265bab798d1450
ywa4you <5ab81c2babf2aabc9985122d2ef11aaf@tutanota.com>
2023-08-11 17:01:30 +0200

    Add the skeleton of the game

--- game_of_life.py
+++ game_of_life.py
@@ -1,3 +1,58 @@
 # Python implementation of Conway's Game of Life
 
+
+from colorama import Fore, Back, Style
+import time
+
+
+def calc_next_field(field):
+    new_field = [[0,] * len(field[0]),] * len(field) 
+
+    for rowi in range(len(field)):
+        for coloumni in range(0, len(field[rowi])):
+            if False:
+                new_field[rowi][coloumni] = 1
+
+    new_field = field
+    return new_field
+    
+
+def draw_field(field, gen):
+    width = coloumns * 2 + 2
+    print("-" * int((width - 16) / 2), end="")
+    print("Generation-" + str(gen).zfill(5), end="")
+    print("-" * int((width - 16) / 2))
+    for row in field:
+        print("|", end="")
+        for coloumn in row:
+            if coloumn == 1:
+                print(Back.GREEN + "  ", end="")
+                print(Style.RESET_ALL, end="")
+            else:
+                print("  ", end="")
+        print("|")
+    print("-" * width)
+
+
+if __name__ == "__main__":
+
+    # define the desired values
+    rows = 10
+    coloumns = 10
+    gens = 10
+
+    # generate field
+    cur_field = []
+    for row in range(rows):
+        cur_field.append([0,] * coloumns)
+
+    # define the pattern
+    cur_field[3][8] = 1
+
+    # print the fields of the generations
+    for gen in range(gens):
+        draw_field(cur_field, gen)
+        cur_field = calc_next_field(cur_field)
+
+
 print("Hello, World!")
