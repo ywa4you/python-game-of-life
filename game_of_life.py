f12c4a08879baefcb07e3e96e9e449b03a5a2712
Defarer <kindruk.difarer@gmail.com>
2023-08-11 18:44:58 +0300

    Add support for checking game rules for a cell

--- game_of_life.py
+++ game_of_life.py
@@ -1,3 +1,22 @@
 # Python implementation of Conway's Game of Life
 
 print("Hello, World!")
+def is_alive(y, x):
+    count = 0
+    for yf in range(-1, 2):
+        for xf in range(-1, 2):
+            print(yf,xf)
+  
+            if field[y+yf][x+xf] == 1:
+                if yf != 0 and xf != 0:
+                    count += 1
+                
+    if count <= 1:
+        return False
+    elif count == 2 or count == 3:
+        return True
+    elif count > 3:
+        return False
+print(is_alive(1,1))
+
+    
