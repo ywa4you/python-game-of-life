4ca85b308f982d687537a5c38e0156a1b986ba64
ywa4you <5ab81c2babf2aabc9985122d2ef11aaf@tutanota.com>
2023-08-12 11:00:19 +0200

    Add a function for calculating the next generation according to is_alive()

--- game_of_life.py
+++ game_of_life.py
@@ -6,22 +6,52 @@ import time
 
 
 def calc_next_field(field):
-    new_field = [[0,] * len(field[0]),] * len(field) 
+    field = field
+    rows = len(field)
+    coloumns = len(field[0])
 
-    for rowi in range(len(field)):
-        for coloumni in range(0, len(field[rowi])):
-            if False:
-                new_field[rowi][coloumni] = 1
+    # generate new field
+    new_field = []
+    for row in range(rows):
+        new_field.append([0,] * coloumns)
+    
+    # add left and right edge
+    for row in range(rows):
+        field[row].insert(0, field[row][-1])
+        field[row].append(field[row][0])
 
-    new_field = field
+    # add top and bottom edge
+    top = []
+    bottom = []
+    for coloumn in range(coloumns+2):
+        top.append(field[0][coloumn])
+        bottom.append(field[-1][coloumn])
+    field.insert(0, top)
+    field.append(bottom)
+
+
+    # fill the field with new values
+    for y in range(1, rows+1):
+        for x in range(1, coloumns+1):
+            if is_alive(y, x, field):
+                new_field[y-1][x-1] = 1
+            else:
+                new_field[y-1][x-1] = 0
     return new_field
     
 
-def draw_field(field, gen):
-    width = coloumns * 2 + 2
-    print("-" * int((width - 16) / 2), end="")
-    print("Generation-" + str(gen).zfill(5), end="")
-    print("-" * int((width - 16) / 2))
+def draw_field(field, gen=0):
+    if gen != 0:
+        width = coloumns * 2 + 2
+        print("-" * int((width - 16) / 2), end="")
+        print("generation-" + str(gen).zfill(5), end="")
+        print("-" * int((width - 16) / 2))
+    else:
+        width = coloumns * 2 + 2
+        print("-" * int((width - 18) / 2), end="")
+        print("initial-generation", end="")
+        print("-" * int((width - 18) / 2))
+
     for row in field:
         print("|", end="")
         for coloumn in row:
@@ -34,12 +64,31 @@ def draw_field(field, gen):
     print("-" * width)
 
 
+def is_alive(y, x, field):
+    count = 0
+
+    for yf in range(-1, 2):
+        for xf in range(-1, 2):
+            if field[y+yf][x+xf] == 1:
+                if yf != 0 and xf != 0:
+                    count += 1
+
+    # uncomment the print() statement for debugging
+    #print(y, x, count)
+    if count <= 1:
+        return False
+    elif count == 2 or count == 3:
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
 
     # define the desired values
-    rows = 10
-    coloumns = 10
-    gens = 10
+    rows = 27
+    coloumns = 49
+    last_gen = 10
 
     # generate field
     cur_field = []
@@ -47,31 +96,29 @@ if __name__ == "__main__":
         cur_field.append([0,] * coloumns)
 
     # define the pattern
-    cur_field[3][8] = 1
+    # cur_field[3][1] = 1
+    # cur_field[3][2] = 1
+    # cur_field[3][3] = 1
+    # cur_field[2][3] = 1
+    # cur_field[1][2] = 1
+
+    #test corners
+    cur_field[0][0] = 1
+    cur_field[0][coloumns-1] = 1
+    cur_field[rows-1][0] = 1
+    cur_field[rows-1][coloumns-1] = 1
+
+    #test shape
+    cur_field[rows // 2+1][coloumns // 2] =1
+    cur_field[rows // 2][coloumns // 2] =1
+    cur_field[rows // 2-1][coloumns // 2] =1
 
     # print the fields of the generations
-    for gen in range(gens):
-        draw_field(cur_field, gen)
+    draw_field(cur_field)
+    for gen in range(1, last_gen+1):
+        time.sleep(0.5)
+        print()
         cur_field = calc_next_field(cur_field)
+        draw_field(cur_field, gen)
 
 
-print("Hello, World!")
-def is_alive(y, x):
-    count = 0
-    for yf in range(-1, 2):
-        for xf in range(-1, 2):
-            print(yf,xf)
-  
-            if field[y+yf][x+xf] == 1:
-                if yf != 0 and xf != 0:
-                    count += 1
-                
-    if count <= 1:
-        return False
-    elif count == 2 or count == 3:
-        return True
-    elif count > 3:
-        return False
-print(is_alive(1,1))
-
-    
